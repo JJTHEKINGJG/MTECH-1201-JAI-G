6a2161322343945118287c964bc70617d1beffad
JJTHEKINGJG <jaiigass12@gmail.com>
2026-02-16 13:12:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+// Jai G \\ 
+// PROJECT 1 SHAPES
+
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(220);
+  background(181,186,12);
+
+strokeWeight(10)
+fill(220)
+line(20,60,100,200)
+
+strokeWeight(10)
+fill(0,255,255)
+rect(0,80,120,50)
+
+strokeWeight(10)
+fill(255,0,0)
+triangle(200,300,250,120,400,20)
+
+
+
+
+
 }
